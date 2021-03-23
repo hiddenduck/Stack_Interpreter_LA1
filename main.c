63c2f59ff2d263925dbbdd2ebaf5e5dccdc12647
Lucenix <andrelucenaferreira@gmail.com>
2021-03-23 20:20:23 +0000

    added Lucena's part of main

---
+++ main.c
@@ -0,0 +1,11 @@
+
+/**
+ * @file
+ * Ficheiro que diz respeito ao main.
+ */
+#include "main.h"
+
+/**
+ * \brief Esta é a função inicial do programa que recebe o \a input.
+ *
+ *  É inicializada uma estrutura STACK através da função CreateStack(), provenientes de stack.h.
