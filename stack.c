61440337c77d2d76e7477e0bbd3ec367ad2ffa96
Lucenix <andrelucenaferreira@gmail.com>
2021-04-13 22:50:09 +0100

    added stack.c stack.h

--- stack.c
+++ stack.c
@@ -1,7 +1,7 @@
 
 /**
  * @file
- * Ficheiro relativo à estrutura STACK e às funções correspondentes.
+ * Ficheiro relativo à estrutura Stack e às funções correspondentes.
  */
 #include "stack.h"
 
@@ -12,9 +12,12 @@
  *
  * @return A Stack inicializada na função.
  */
-STACK CreateStack() {
-    STACK s;
-    s.sp = -1;
+Stack *CreateStack() {
+    Stack *s = (Stack *) malloc(sizeof(Stack));
+    assert(s != NULL);
+    s->size = INCREMENTO_STACK;
+    s->sp = -1;
+    s->array = (Data *) calloc(s->size, sizeof(Data));
     return s;
 }
 
@@ -22,20 +25,59 @@ STACK CreateStack() {
  * \brief Função que devolve o elemento na posição sp da \a stack e decrementa sp.
  *
  * @param stack Endereço da \a stack responsável pelo armazenamento.
- * @return Inteiro do tipo long na posição do ponteiro do stack.
+ * @return Devolve o elemento do tipo Data na última posiçao.
  */
-long Pop(STACK *stack) {
+Data Pop(Stack *stack) {
     return stack->array[(stack->sp)--];
 }
 
 /**
  * \brief Função que aumenta o sp da \a stack e coloca um elemento nessa posição.
  *
- * @param val Inteiro do tipo long a colocar na stack.
+ * @param data Elemento do tipo Data a colocar na \a stack.
  * @param stack Endereço da \a stack responsável pelo armazenamento.
  */
-void Push(long val, STACK *stack) {
-    stack->array[++(stack->sp)] = val;
+void Push(Data data, Stack *stack) {
+    if(stack->size == (stack->sp)-1) {
+        stack->size +=INCREMENTO_STACK;
+        stack->array = (Data *) realloc(stack->array, stack->size * sizeof(Data));
+    }
+    stack->sp++;
+    stack->array[(stack->sp)] = data;
+}
+
+/**
+ * \brief Função que lê um elemento a uma certa distância do sp.
+ *
+ * @param deslocamento Distância em relação ao sp.
+ * @param stack Endereço da \a stack responsável pelo armazenamento.
+ * @return Devolve um elemento do tipo Data.
+ */
+Data *Read(long deslocamento, Stack *stack) {
+    Data *val = &stack->array[stack->sp-deslocamento];
+    return val;
+}
+
+/**
+ * \brief Função que imprime um certo elemento de acordo com o seu Tipo.
+ *
+ * @param data Elemento a imprimir.
+ */
+void PrintData(Data *data) {
+    switch (data->tipo) {
+        case LONG:
+            printf("%ld", *(long*)(data->value));
+            break;
+        case DOUBLE:
+            printf("%g", *(double*)(data->value));
+            break;
+        case CHAR:
+            printf("%c", *((char*)(data->value)));
+            break;
+        case STRING:
+            printf("%s", (char*)data->value);
+            break;
+    }
 }
 
 /**
@@ -43,9 +85,125 @@ void Push(long val, STACK *stack) {
  *
  * @param stack Endereço da \a stack responsável pelo armazenamento.
  */
-void PrintStack(STACK *stack) {
+void PrintStack(Stack *stack) {
     for (int i = 0 ; i <= stack -> sp; i++){
-        printf("%ld" , stack -> array[i]);
+        PrintData(&stack->array[i]);
     }
     putchar('\n');
+}
+
+/**
+ * \brief Macro que assiste na criação das funções DataVal.
+ * place holder
+ * 
+ */
+#define DataVal(_name, _type)              \
+    _type *DataVal##_name(Data *data) {    \
+        return (_type*) data->value;       \
+    }
+
+DataVal(CHAR, char)
+DataVal(LONG, long)
+DataVal(DOUBLE, double)
+DataVal(STRING, char)
+
+/** blank */
+#define CreateData(_name, _type)                       \
+    Data CreateData##_name(_type val) {                \
+        _type *vp = (_type*) malloc(sizeof(_type));    \
+        *vp = val;                                     \
+        Data op = {_name, vp};                         \
+        return op;                                     \
+    }
+/** blank */
+CreateData(CHAR, char)
+/** blank */
+CreateData(LONG, long)
+/** blank */
+CreateData(DOUBLE, double)
+/** blank */
+Data CreateDataSTRING(char *val) {
+    char *vp = strdup(val);
+    Data op = {STRING, vp};
+    return op;
+}
+
+/**
+ * \brief Função que converte um Data com qualquer Tipo num Data com Tipo DOUBLE.
+ * @param d Endereço de um Data.
+ */
+void DataToDOUBLE(Data *d) {
+    double val;
+    switch (d->tipo){
+    case CHAR:
+    case LONG:{
+        val = (double) *(DataValLONG(d));
+        *d = CreateDataDOUBLE(val);
+        break;
+    }
+    case STRING:{
+        val = strtod(DataValCHAR(d), NULL);
+        *d = CreateDataDOUBLE(val);
+        break;
+    }
+    default:
+        d->tipo = DOUBLE;
+        break;
+    }
+}
+
+/**
+ *\brief Função que converte um Data com qualquer Tipo num Data com Tipo LONG.
+ * @param d Endereço de um Data
+ */
+void DataToLONG(Data *d) {
+    long val;
+    switch (d->tipo){
+        case STRING:{
+            val = strtol(DataValCHAR(d), NULL, 10);
+            *d = CreateDataLONG(val);
+            break;
+        }
+        case DOUBLE: {
+            val = (long) *(DataValDOUBLE(d));
+            *d = CreateDataLONG(val);
+            break;
+        }
+        default:
+            d->tipo = LONG;
+            break;
+    }
+}
+
+/**
+ * \brief Função que duplica um Data.
+ *
+ * @param target Endereço de um Data.
+ * @return
+ */
+Data DataDup(Data *target) {
+    Data data;
+    switch (target->tipo) {
+        case LONG: {
+            long val = *DataValLONG(target);
+            data = CreateDataLONG(val);
+            break;
+        }
+        case DOUBLE: {
+            double val = *DataValDOUBLE(target);
+            data = CreateDataDOUBLE(val);
+            break;
+        }
+        case CHAR: {
+            char val = *DataValCHAR(target);
+            data = CreateDataCHAR(val);
+            break;
+        }
+        case STRING: {
+            char* val = DataValSTRING(target);
+            data = CreateDataSTRING(val);
+            break;
+        }
+    }
+    return data;
 }
--- stack.h
+++ stack.h
@@ -1,33 +1,67 @@
 
 /**
- * @file
+ * @headerfile
  */
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 /**
- * \brief Tamanho máximo do array de uma stack.
+ * \brief Definição do incremento da stack.
  */
+#define INCREMENTO_STACK 100
 
-#define MAX_LENGTH 10240
 /**
- * \brief Estrutura STACK para representar uma stack.
- * Definida por um array do tipo long e por um inteiro sp.
+ * \brief São enunciados os tipos possiveis.
  */
-typedef struct {
-    /**
-     * \param array Array do tipo long de tamanho MAX_LENGTH para armazenar.
-     */
-    long array[MAX_LENGTH];
-    /**
-     * \param sp Inteiro que representa o indíce do último elemento da stack.
-     */
+typedef enum {CHAR = 1, LONG = 2, DOUBLE = 4, STRING = 8} Tipo;
+
+/**
+ * \brief É criado o tipo de dados Data.
+ */
+typedef struct data {
+    Tipo tipo;
+    void* value;
+}Data;
+
+/**
+ * \brief É criado o tipo de dados Stack
+ */
+typedef struct stack {
+    Data *array;
+    int size;
     int sp;
-}STACK;
+}Stack;
 
-STACK CreateStack();
 
-long Pop(STACK *stack);
+Stack *CreateStack();
 
-void Push(long val, STACK *stack);
+Data Pop(Stack *stack);
+void Push(Data operando, Stack *stack);
+Data *Read(long deslocamento, Stack *stack);
 
-void PrintStack(STACK *stack);
+void PrintStack(Stack *stack);
+
+/** 
+ * \brief Macro que assiste na inicialização das funcões DataVal. 
+*/
+#define DataValProto(_name, _type)       \
+    _type *DataVal##_name(Data *val);    \
+
+DataValProto(LONG, long)
+DataValProto(DOUBLE, double)
+DataValProto(CHAR, char)
+DataValProto(STRING, char)
+
+/** 
+ * \brief Macro que assiste na inicialização das funções CreateData. 
+*/
+#define CreateDataProto(_name, _type)    \
+    Data CreateData##_name(_type val);   \
+
+CreateDataProto(LONG, long)
+CreateDataProto(DOUBLE, double)
+CreateDataProto(CHAR, char)
+CreateDataProto(STRING, char *)
