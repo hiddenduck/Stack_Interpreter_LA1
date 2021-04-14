751f2672e99bf918ac1b1f650e0c102722c9ea52
Lucenix <andrelucenaferreira@gmail.com>
2021-04-14 10:30:10 +0100

    começar grandeza

--- parser.c
+++ parser.c
@@ -12,6 +12,164 @@
  * @param input String com o \a input.
  * @param stack Endereço da \a stack responsável pelo armazenamento.
  */
+void operate(char *token, Stack *stack) {
+    if(strlen(token)==1) {
+        switch (token[0]) {
+            case '+': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                soma(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '-': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                subtr(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '*': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                mult(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '/': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                divi(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '(': {
+                Data x = Pop(stack);
+                decre(&x);
+                Push(x, stack);
+                break;
+            }
+            case ')': {
+                Data x = Pop(stack);
+                incre(&x);
+                Push(x, stack);
+                break;
+            }
+            case '%': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                modulo(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '#': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                potencia(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '&': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                and(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '|': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                or(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '^': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                xor(&x, &y);
+                Push(y, stack);
+                free(x.value);
+                break;
+            }
+            case '~': {
+                Data x = Pop(stack);
+                not(&x);
+                Push(x, stack);
+                break;
+            }
+            case ';': {
+                Pop(stack);
+                break;
+            }
+            case '_': {
+                Data *x = Read(0, stack);
+                Data y = DataDup(x);
+                Push(y, stack);
+                break;
+            }
+            case '\\': {
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                Push(y, stack);
+                Push(x, stack);
+                break;
+            }
+            case '@': {
+                Data z = Pop(stack);
+                Data y = Pop(stack);
+                Data x = Pop(stack);
+                Push(y, stack);
+                Push(z, stack);
+                Push(x, stack);
+                break;
+            }
+            case '$': {
+                Data indice = Pop(stack);
+                long deslocamento = *DataValLONG(&indice);
+                Data *x = Read(deslocamento, stack);
+                Data y = DataDup(x);
+                Push(y, stack);
+                free(indice.value);
+                break;
+            }
+            case 'c': {
+                Data x = Pop(stack);
+                DataToLONG(&x);
+                x.tipo = CHAR;
+                Push(x, stack);
+                break;
+            }
+            case 'l': {
+                char linha[MAX_LENGTH_INPUT];
+                assert(fgets(linha, MAX_LENGTH_INPUT, stdin) != NULL);
+                assert(linha[strlen(linha) - 1] == '\n');
+                Push(CreateDataSTRING(linha), stack);
+                break;
+            }
+            case 'i': {
+                Data x = Pop(stack);
+                DataToLONG(&x);
+                Push(x, stack);
+                break;
+            }
+            case 'f':{
+                Data x = Pop(stack);
+                DataToDOUBLE(&x);
+                Push(x, stack);
+                break;
+            }
+        }
+    }
+}
+
 void parse(char *input, Stack *stack){
 	char *delims = " \t\n";
 	for(char *token = strtok(input, delims); token != NULL; token = strtok(NULL, delims)){
@@ -25,168 +183,16 @@ void parse(char *input, Stack *stack){
 		    /* Testar se o resto contém um double decimal e somar à parte inteira. */
 		    double vald = strtod(resto, &resto);
 		    
-            if (vall < 0) vald = vall - vald;
-		    else vald += vall;
+            if (vall < 0)
+                vald = vall - vald;
+		    else
+		        vald += vall;
 
-		    if (strlen(resto) ==  0) {
-                    Push(CreateDataDOUBLE(vald), stack);
-		    } else {
-                if(strlen(token)==1) {
-                    switch (token[0]) {
-                        case '+': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            soma(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '-': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            subtr(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '*': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            mult(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '/': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            divi(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '(': {
-                            Data x = Pop(stack);
-                            decre(&x);
-                            Push(x, stack);
-                            break;
-                        }
-                        case ')': {
-                            Data x = Pop(stack);
-                            incre(&x);
-                            Push(x, stack);
-                            break;
-                        }
-                        case '%': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            modulo(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '#': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            potencia(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '&': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            and(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '|': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            or(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '^': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            xor(&x, &y);
-                            Push(y, stack);
-                            free(x.value);
-                            break;
-                        }
-                        case '~': {
-                            Data x = Pop(stack);
-                            not(&x);
-                            Push(x, stack);
-                            break;
-                        }
-                        case ';': {
-                            Pop(stack);
-                            break;
-                        }
-                        case '_': {
-                            Data *x = Read(0, stack);
-                            Data y = DataDup(x);
-                            Push(y, stack);
-                            break;
-                        }
-                        case '\\': {
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            Push(y, stack);
-                            Push(x, stack);
-                            break;
-                        }
-                        case '@': {
-                            Data z = Pop(stack);
-                            Data y = Pop(stack);
-                            Data x = Pop(stack);
-                            Push(y, stack);
-                            Push(z, stack);
-                            Push(x, stack);
-                            break;
-                        }
-                        case '$': {
-                            Data indice = Pop(stack);
-                            long deslocamento = *DataValLONG(&indice);
-                            Data *x = Read(deslocamento, stack);
-                            Data y = DataDup(x);
-                            Push(y, stack);
-                            free(indice.value);
-                            break;
-                        }
-                        case 'c': {
-                            Data x = Pop(stack);
-                            DataToLONG(&x);
-                            x.tipo = CHAR;
-                            Push(x, stack);
-                            break;
-                        }
-                        case 'l': {
-                            char linha[MAX_LENGTH_INPUT];
-                            assert(fgets(linha, MAX_LENGTH_INPUT, stdin) != NULL);
-                            assert(linha[strlen(linha) - 1] == '\n');
-                            Push(CreateDataSTRING(linha), stack);
-                            break;
-                        }
-                        case 'i': {
-                            Data x = Pop(stack);
-                            DataToLONG(&x);
-                            Push(x, stack);
-                            break;
-                        }
-                        case 'f':{
-                            Data x = Pop(stack);
-                            DataToDOUBLE(&x);
-                            Push(x, stack);
-                            break;
-                        }
-                    }
-                }
-		    }
+		    if (strlen(resto) ==  0)
+		        Push(CreateDataDOUBLE(vald), stack);
+		    else
+		        operate(token, stack);
+
 		}
 	}
 	PrintStack(stack);
